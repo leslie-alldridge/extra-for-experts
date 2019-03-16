4c1e88a575adec4ed38a3a51c4ff2103c348f068
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-17 09:02:32 +1300

    set up week1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    week: 0
+  };
+
+  handleClick = () => {};
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Please select a week from the list below</h2>
+          <ul>
+            <li onClick={this.handleClick} id="listItem">
+              Week 1
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 2
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 3
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 4
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 5
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 6
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 7
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 8
+            </li>
+            <li onClick={this.handleClick} id="listItem">
+              Week 9
+            </li>
+          </ul>
         </header>
       </div>
     );
---
+++ src/components/week1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const week1 = () => {
+  return (
+    <div>
+      <h2>Welcome to week one</h2>
+    </div>
+  );
+};
