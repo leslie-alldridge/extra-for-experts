b884eb7ca47a61c0a2e61bc3ba6e1ade956ca13d
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-17 10:18:31 +1300

    w6

--- src/components/week6.jsx
+++ src/components/week6.jsx
@@ -3,32 +3,23 @@ import React from "react";
 const Week6 = () => {
   return (
     <div>
-      <h2>Week 1 Bonus Exercises</h2>
+      <h2>Week 6 Bonus Exercises</h2>
+      <p>Week six is probably the hardest week in the entire course.</p>
       <p>
-        Week one is a lot of Javascript and in some cases basic Express routing.
-      </p>
-      <p>
-        Before practising, you should have all exercises for the week completed.
-        Following this you can:
+        Please push hard as this is pretty much the end of bootcamp. Following
+        this you have authentication and two group projects.
       </p>
       <ul>
         <li>
-          Create an Express server with routes. So that the route /game/1
-          returns the score of the first game in bowling. /game/2 returns the
-          second, and so on. From there, you can add /games which will show all
-          games and /help to display a help page.
+          Complete sweet as beers INCLUDING an admin page where you can view all
+          orders that have been made. The orders should be safe and sound in a
+          knex database, no longer using the data.json file.
         </li>
         <hr />
         <li>
-          Create an Express web server (any context is fine) but it must contain
-          routes that match the following layouts.
-          <br /> /names <br />
-          /name/:id
-          <br />
-          /name/edit/:id
-          <br /> <br />
-          and a bonus....to show names in a particular region and suburb <br />
-          /name/:region/:suburb
+          Once you've completed the above, delete it all and start again. Sweet
+          as beers is by far the best exercise and it should be completed 2-4
+          times at least.
         </li>
       </ul>
     </div>
